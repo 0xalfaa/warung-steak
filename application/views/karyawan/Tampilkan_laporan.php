d378f7b45d37f78e9f79736c68f485e25c89b64d
0xalfaa <callmealfaa@gmail.com>
2024-06-12 23:27:43 +0800

    "Refactored HTML structure in Tampilkan_laporan.php view, adding card and grid-margin elements, and rearranging form and table elements."

--- application/views/karyawan/Tampilkan_laporan.php
+++ application/views/karyawan/Tampilkan_laporan.php
@@ -4,52 +4,58 @@
             <h1>Laporan Penjualan</h1>
         </div>
 
-    <form action="<?php echo base_url('admin/laporan') ?>" method="POST">
-        <div class="form-group">
-            <label>Dari Tanggal</label>
-            <input type="date" name="dari" class="form-control">
-            <?php echo form_error('dari', '<span class="text-small text-danger">','</span>') ?>
+        <div class="grid-margin">
+            <div class="card">
+            <div class="card-body">
+                <form action="<?php echo base_url('admin/laporan') ?>" method="POST">
+                    <div class="form-group">
+                        <label>Dari Tanggal</label>
+                        <input type="date" name="dari" class="form-control">
+                        <?php echo form_error('dari', '<span class="text-small text-danger">','</span>') ?>
 
-        </div>
-        <div class="form-group">karyawan
-            <label>Sampai Tanggal</label>
-            <input type="date" name="sampai" class="form-control">
-            <?php echo form_error('sampai', '<span class="text-small text-danger">','</span>') ?>
+                    </div>
+                    <div class="form-group">karyawan
+                        <label>Sampai Tanggal</label>
+                        <input type="date" name="sampai" class="form-control">
+                        <?php echo form_error('sampai', '<span class="text-small text-danger">','</span>') ?>
 
-        </div>
+                    </div>
 
-        <button type="submit" class="btn btn-sm btn-primary">Tampilkan Data</button>
-    
-    </form><hr>
+                    <button type="submit" class="btn btn-sm btn-primary">Tampilkan Data</button>
+                
+                </form><hr>
 
-    <div class="btn-group">
-        <a class="btn btn-sm btn-success" target="_blank" href="<?php echo base_url().'karyawan/laporan/print_laporan/?dari='.set_value('dari').'&sampai='.set_value('sampai') ?>"><i class="fas fa-print"></i> Print</a>
-    </div>
+                <div class="btn-group">
+                    <a class="btn btn-sm btn-success" target="_blank" href="<?php echo base_url().'karyawan/laporan/print_laporan/?dari='.set_value('dari').'&sampai='.set_value('sampai') ?>"><i class="fas fa-print"></i> Print</a>
+                </div>
 
-        <div class="table-responsive mt-3">
-            <table class="table table-bordered table-striped">
-                <tr>
-                    <th>No</th>
-                    <th>Tanggal</th>
-                    <th>Nama Menu</th>
-                    <th>Harga</th>
-                    <th>Jumlah</th>
-                    <th>Total</th>
-                    <th>Nama Karyawan</th>
-                </tr>
+                    <div class="table-responsive mt-3">
+                        <table class="table table-bordered table-striped">
+                            <tr>
+                                <th>No</th>
+                                <th>Tanggal</th>
+                                <th>Nama Menu</th>
+                                <th>Harga</th>
+                                <th>Jumlah</th>
+                                <th>Total</th>
+                                <th>Nama Karyawan</th>
+                            </tr>
 
-                <?php $no = 1; foreach($laporan as $l) : ?>
-                    <tr>
-                        <td><?php echo $no++ ?></td>
-                        <td><?php echo $l->TANGGAL ?></td>
-                        <td><?php echo $l->NAMA_MENU ?></td>
-                        <td>Rp. <?php echo number_format($l->HARGA, 0, ',', '.') ?></td>
-                        <td><?php echo $l->JUMLAH ?></td>
-                        <td>Rp. <?php echo number_format($l->TOTAL, 0, ',', '.') ?></td>
-                        <td><?php echo $l->NAMA_KARYAWAN ?></td>
-                    </tr>
-                <?php endforeach; ?>
-            </table>
-        </div>
+                            <?php $no = 1; foreach($laporan as $l) : ?>
+                                <tr>
+                                    <td><?php echo $no++ ?></td>
+                                    <td><?php echo $l->TANGGAL ?></td>
+                                    <td><?php echo $l->NAMA_MENU ?></td>
+                                    <td>Rp. <?php echo number_format($l->HARGA, 0, ',', '.') ?></td>
+                                    <td><?php echo $l->JUMLAH ?></td>
+                                    <td>Rp. <?php echo number_format($l->TOTAL, 0, ',', '.') ?></td>
+                                    <td><?php echo $l->NAMA_KARYAWAN ?></td>
+                                </tr>
+                            <?php endforeach; ?>
+                        </table>
+                    </div>
+                </div>
+            </div>
+         </div>
     </section>
 </div>
